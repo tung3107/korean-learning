0146ad07ca929f06d5090c53fa0830bc8d9935af
Dương Tùng <Dng Tng@TUNG-DIX>
2025-02-24 00:20:05 +0700

    Add again

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -24,8 +24,7 @@ const sentTokenCookie = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
-    httpOnly: true,
-    sameSite: 'lax', // jwt cannot be modified
+    httpOnly: true, // jwt cannot be modified
   };
   if (process.env.NODE_ENV === 'production') cookiesOptions.secure = true; // can only access http
 
